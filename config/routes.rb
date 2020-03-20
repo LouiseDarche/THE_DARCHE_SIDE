fbac8582b79caea6f36f4b086fef5c5be9b4f041
Louise Darche <louise.darche@gmail.com>
2020-03-20 16:32:31 +0100

    desktop ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # get 'home/index'
   root to: 'pages#home'
   resources :visitors, only: [:new, :create]
-  get 'hardtofind/visitors', to: "visitors#index", as: :index
+  # get 'hardtofind/visitors', to: "visitors#index", as: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
